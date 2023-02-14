918ad80434b53e172a3d742fa7b9432af116ef47
aleesan7 <ka_sanchezsamayoa@outlook.com>
2023-02-13 22:45:28 -0600

    Version inicial

---
+++ webcamapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Camera from "./AllCameras";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Semi 1 Tarea 2</h2>
+        <h2>Kevin Alejandro Sanchez Samayoa - 201114336</h2>
+        <Camera />
+      </header>
+    </div>
+  );
+}
+
+export default App;
